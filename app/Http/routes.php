47dcdaeb9698deccdf94100f13cfd91ca53b1767
Cassiszuoan <jerry831210@gmail.com>
2016-08-12 00:26:05 +0800

    add picture route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('uploads/{filename}', function ($filename)
+{
+    $path = storage_path() . '/' . $filename;
+
+    if(!File::exists($path)) abort(404);
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
