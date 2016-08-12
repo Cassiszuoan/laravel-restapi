4885ad3ed5c3cdbe7b94db520e24aa8d66f36196
Cassiszuoan <jerry831210@gmail.com>
2016-08-12 14:58:44 +0800

    wtf

--- app/Api/V1/Controllers/UserController.php
+++ app/Api/V1/Controllers/UserController.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Password;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Dingo\Api\Exception\ValidationHttpException;
 use App\Api\V1\Transformers\UserTransformer;
+use Intervention\Image\ImageManager;
 
 class UserController extends BaseController
 {
