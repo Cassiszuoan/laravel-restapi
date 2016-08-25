44763980df0bb395f55fcfed9c3109c895e23c8d
Cassiszuoan <jerry831210@gmail.com>
2016-08-25 16:00:38 +0800

    testing

--- app/Api/V1/Controllers/PostController.php
+++ app/Api/V1/Controllers/PostController.php
@@ -108,11 +108,11 @@ if(file_exists($uploadfile)){
   unlink($uploadfile);
 
   if (move_uploaded_file($_FILES['pic']['tmp_name'], $uploadfile)) {
-
+   $imgurl = "http://140.136.155.143/". $uploadfile;
    $post->imgurl =  "http://140.136.155.143/". $uploadfile;
    $post->save();
 
-   $img = Image::make("http://140.136.155.143/". $uploadfile)->resize(300, 300)->insert($uploadfile . '_300*300');
+   $img = Image::make($imgurl);
    $array = array ("code" => "1", "message" => "successfully","url"=>"140.136.155.143/". $uploadfile);  
 } else {
    $array = array ("code" => "0", "message" => "Possible file upload attack!".$_FILES['pic']['name']); 
@@ -127,7 +127,7 @@ else{
 if (move_uploaded_file($_FILES['pic']['tmp_name'], $uploadfile)) {
    $post->imgurl =  "http://140.136.155.143/". $uploadfile;
    $post->save();
-   $img = Image::make("http://140.136.155.143/". $uploadfile)->resize(300, 300)->insert($uploadfile . '_300*300');
+   $img = Image::make($imgurl);
    $array = array ("code" => "1", "message" => "successfully","url"=>"140.136.155.143/". $uploadfile);  
 } else {
    $array = array ("code" => "0", "message" => "Possible file upload attack!".$_FILES['pic']['name']); 
